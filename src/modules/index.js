7f648b2a00dfbc2928ccdde35ab1d7aec3bf2ed3
Ariful Alam <alamariful1727@gmail.com>
2020-02-12 13:03:39 +0600

    initial commit for this branch

--- src/modules/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var AuthComponent = require("./auth/auth.controller");
-exports.AuthComponent = AuthComponent;
-var UserComponent = require("./user/user.controller");
-exports.UserComponent = UserComponent;
